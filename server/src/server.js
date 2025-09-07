0aefcd9e319fdc9aae19fbdf264bf3c51834803d
soumadip-dev <soumadipmajila@gmail.com>
2025-09-07 21:48:26 +0530

    chore: setup backend with Express

---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//* Root Route
+app.get('/', (req, res) => {
+  res.send('Hello from BiteBox backend!');
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
