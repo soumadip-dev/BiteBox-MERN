4d20064a68295a44ac7756ff415e9d587e6f1dbe
soumadip-dev <soumadipmajila@gmail.com>
2025-09-12 12:40:00 +0530

    feat: set up page routing for SignIn and SignUp using React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/signUp';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/signUp" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
