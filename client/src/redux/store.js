19c3225b6b3fa414b3adf0fdcba7237889e64bb5
soumadip-dev <soumadipmajila@gmail.com>
2025-09-14 13:40:00 +0530

    docs: add explanatory comments throughout the project for better readability

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice.js';
 
+//* Configure the Redux store
 export const store = configureStore({
   reducer: {
     user: userReducer,
--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+//* Initial state for user
 const initialState = {
   userData: null,
 };
 
+//* Create a Redux slice for user
 export const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -14,5 +16,6 @@ export const userSlice = createSlice({
   },
 });
 
+//* Export actions and reducer
 export const { setUserData } = userSlice.actions;
 export default userSlice.reducer;
