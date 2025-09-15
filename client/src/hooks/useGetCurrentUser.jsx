d611fabc50ef30816d7b08e7e99c282c96223b0c
soumadip-dev <soumadipmajila@gmail.com>
2025-09-15 12:20:00 +0530

    feat: update useGetCurrentUser hook to dispatch user data to Redux store

--- client/src/hooks/useGetCurrentUser.jsx
+++ client/src/hooks/useGetCurrentUser.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react';
 import { getCurrentUser } from '../api/userApi';
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice.js';
 
 //* Custom hook to get the current user
 const useGetCurrentUser = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const response = await getCurrentUser();
+        dispatch(setUserData(response.user));
         if (response.success) {
           setUser(response.user);
         } else {
