ae9d39b52f6f3f664d14d734dd2c49cccfddf0a7
soumadip-dev <soumadipmajila@gmail.com>
2025-09-16 13:50:00 +0530

    fix: make hardcoded city dynamic in mobile view Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,14 +40,14 @@ const Navbar = () => {
       setIsLoading(false);
     }
   };
-  
+
   return (
     <div className="w-full h-[80px] flex items-center justify-between md:justify-center gap-[30px] px-[20px] fixed top-0 z-[9999]bg-[#fff9f6] overflow-visible">
       {showSearch && (
         <div className="w-[90%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] flex fixed top-[80px] left-[5%]">
           <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
             <FaLocationDot size={25} className="text-[#ff4d2d]" />
-            <div className="w-[80%] truncate text-gray-600">Durgapur</div>
+            <div className="w-[80%] truncate text-gray-600">{city}</div>
           </div>
           <div className="w-[80%] flex items-center gap-[10px]">
             <IoIosSearch size={25} className="text-[#ff4d2d]" />
