327612b47692cb97f2116c218fb0cdffaddddfe2
soumadip-dev <soumadipmajila@gmail.com>
2025-09-18 13:10:00 +0530

    feat: add isOtpVerified field to User model

--- server/src/model/user.model.js
+++ server/src/model/user.model.js
@@ -33,6 +33,10 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    isOtpVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
