1eed8a560517435546e9948ae4ad07ea49ec6c8f
soumadip-dev <soumadipmajila@gmail.com>
2025-09-20 13:10:00 +0530

    feat: implement controller for sending password reset email using service and mailgen config

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -1,5 +1,10 @@
-import { registerService, loginService, sendPasswordResetEmailService } from '../services/user.service.js';
+import {
+  registerService,
+  loginService,
+  sendPasswordResetEmailService,
+} from '../services/user.service.js';
 import { ENV } from '../config/env.config.js';
+import { transporter, generateEmail } from '../config/email.config.js';
 
 //* Controller for registering a user
 const registerUser = async (req, res) => {
@@ -86,6 +91,42 @@ const sendPasswordResetEmail = async function (req, res) {
   try {
     // Get the user and otp from sendPasswordResetEmailService
     const { user, otp } = await sendPasswordResetEmailService(email);
+
+    // Send password reset email to user
+    const emailContent = generateEmail({
+      name: user.fullName,
+      intro: 'You have requested to reset your password.',
+      action: {
+        instructions: 'Please use the following OTP to reset your password:',
+        button: {
+          color: '#DC4D2F',
+          text: otp,
+          link: '#',
+          fallback: true,
+        },
+      },
+      outro: 'If you did not request this reset, please ignore this email.',
+    });
+
+    const mailOptions = {
+      from: `${ENV.COMPANY_NAME} <${ENV.BREVO_SENDEREMAIL}>`,
+      to: email,
+      subject: 'Password Reset Request',
+      html: emailContent.html,
+      text: emailContent.text,
+    };
+    try {
+      await transporter.sendMail(mailOptions);
+    } catch (emailError) {
+      return res
+        .status(500)
+        .json({ message: emailError.message || 'Email sending failed', success: false });
+    }
+    // Send success response
+    return res.status(200).json({
+      message: 'Password reset email sent successfully',
+      success: true,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message || 'Something went wrong', success: false });
   }
