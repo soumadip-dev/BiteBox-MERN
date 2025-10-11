d154fa6b1b5891546c8aac85badd65696128cc22
soumadip-dev <soumadipmajila@gmail.com>
2025-10-12 01:02:53 +0530

    feat: add Cloudinary upload utility with environment configuration

---
+++ server/src/config/cloudinary.config.js
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { ENV } from './env.config';
+import fs from 'fs/promises';
+
+const uploadOnCloudinary = async file => {
+  cloudinary.config({
+    cloud_name: ENV.CLOUDINARY_CLOUD_NAME,
+    api_key: ENV.CLOUDINARY_API_KEY,
+    api_secret: ENV.CLOUDINARY_API_SECRET,
+  });
+  try {
+    const { secure_url } = await cloudinary.uploader.upload(file);
+    fs.unlinkSync(file);
+    return secure_url;
+  } catch (error) {
+    fs.unlinkSync(file);
+    console.log(error);
+  }
+};
+
+export default uploadOnCloudinary;
