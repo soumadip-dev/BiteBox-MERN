0120ecf3e9b119d29df0ce86617da3de72eedb67
soumadip-dev <soumadipmajila@gmail.com>
2025-10-23 10:00:00 +0530

    feat: add routes for creating and editing shops

---
+++ server/src/routes/shop.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { createAndEditShop } from '../controller/shop.controller.js';
+import { isAuth } from '../middleware/user.middleware.js';
+
+//* Create a new Express router
+const router = Router();
+
+//* Define routes
+router.get('/create-edit', isAuth, createAndEditShop);
+
+//* Export the router
+export default router;
