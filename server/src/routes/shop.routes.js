0d3d99f5c04d0de081076717a33f5b9d918c63c2
soumadip-dev <soumadipmajila@gmail.com>
2025-10-26 10:00:00 +0530

    feat: add multer middleware to create/edit shop route in shop.routes.js

--- server/src/routes/shop.routes.js
+++ server/src/routes/shop.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { createAndEditShop, getMyShop } from '../controller/shop.controller.js';
 import { isAuth } from '../middleware/user.middleware.js';
+import { upload } from '../middleware/multer.middleware.js';
 
 //* Create a new Express router
 const router = Router();
 
 //* Define routes
-router.post('/create-edit', isAuth, createAndEditShop);
+router.post('/create-edit', isAuth, upload.single('image'), createAndEditShop);
 router.get('/get-my', isAuth, getMyShop);
 
 //* Export the router
