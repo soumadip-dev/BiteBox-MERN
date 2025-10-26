4d6019d423fb6174e125b77e536f9f92b463e33f
soumadip-dev <soumadipmajila@gmail.com>
2025-10-26 10:00:00 +0530

    fix: add 'Content-Type': 'multipart/form-data' header in shopApi.js

--- client/src/api/shopApi.js
+++ client/src/api/shopApi.js
@@ -12,3 +12,23 @@ export const getMyShop = async function () {
     return { success: false, message: 'Network error occurred' };
   }
 };
+
+//* Create shop
+export const createEditShop = async function (shopData) {
+  try {
+    const response = await axiosInstance.post('/api/v1/shop/create-edit', shopData, {
+      headers: {
+        'Content-Type': 'multipart/form-data', // Set the content type
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log('API Error:', error.message);
+    console.log('Error response:', error.response?.data);
+
+    if (error.response && error.response.data) {
+      return error.response.data;
+    }
+    return { success: false, message: 'Network error occurred' };
+  }
+};
