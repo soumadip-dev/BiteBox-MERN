b9435dcfd06c88ed5ce2d974f9ab75dc3950feb5
soumadip-dev <soumadipmajila@gmail.com>
2025-11-01 22:57:04 +0530

    feat: add rating field with average and count to item.model.js

--- server/src/model/item.model.js
+++ server/src/model/item.model.js
@@ -43,6 +43,16 @@ const itemSchema = new mongoose.Schema(
       enum: ['veg', 'non veg'],
       required: true,
     },
+    rating: {
+      average: {
+        type: Number,
+        default: 0,
+      },
+      count: {
+        type: Number,
+        default: 0,
+      },
+    },
   },
   {
     timestamps: true,
